86698f69d765c1c3efc4230dfa80b22145f7496d
roben <yizhang19801001@gmail.com>
2021-01-03 14:29:25 +1100

    click counter app with TDD

--- 02. React Unit Test Basic/src/App.js
+++ 02. React Unit Test Basic/src/App.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div data-testid="component-app">Hello React Hot!</div>;
+  const [counter, setCounter] = useState(0);
+  return (
+    <div data-testid="component-app">
+      <div data-testid="div-counter">{counter}</div>
+      <button
+        data-testid="increase-button"
+        onClick={() => setCounter(counter + 1)}
+      >
+        increase
+      </button>
+      <button
+        data-testid="decrease-button"
+        onClick={() => {
+          if (counter === 0) return;
+          setCounter(counter - 1);
+        }}
+      >
+        increase
+      </button>
+    </div>
+  );
 }
 
 export default App;
--- 02. React Unit Test Basic/src/App.test.js
+++ 02. React Unit Test Basic/src/App.test.js
@@ -1,9 +1,73 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, cleanup, fireEvent } from "@testing-library/react";
 import App from "./App";
 
+beforeEach(() => {
+  render(<App />);
+});
+
+afterEach(() => {
+  cleanup();
+});
+
 test("component render without errors", () => {
-  const { queryByTestId } = render(<App />);
+  const { queryByTestId } = screen;
   const rootDom = queryByTestId("component-app");
   expect(rootDom).toBeInTheDocument();
+});
+
+test("show counter div when component render", () => {
+  const { queryByTestId } = screen;
+  const counterDiv = queryByTestId("div-counter");
+  expect(counterDiv).toBeInTheDocument();
+});
+test("show button increase when component render", () => {
+  const { queryByTestId } = screen;
+  const increaseBtn = queryByTestId("increase-button");
+  expect(increaseBtn).toBeInTheDocument();
+});
+
+test("show button decrease when component render", () => {
+  const { queryByTestId } = screen;
+  const decreaseBtn = queryByTestId("decrease-button");
+  expect(decreaseBtn).toBeInTheDocument();
+});
+
+test("counter is intialized as 0", () => {
+  const { queryByTestId } = screen;
+  const counterDiv = queryByTestId("div-counter");
+  expect(counterDiv).toHaveTextContent("0");
+});
+
+test("when click increase button, counter increase by 1", () => {
+  const { queryByTestId } = screen;
+  const increaseBtn = queryByTestId("increase-button");
+  const counterDiv = queryByTestId("div-counter");
+  fireEvent.click(increaseBtn);
+
+  expect(counterDiv).toHaveTextContent("1");
+});
+
+test("when click decrease button, counter decrease by 1", () => {
+  const { queryByTestId } = screen;
+  const increaseBtn = queryByTestId("increase-button");
+  const decreaseBtn = queryByTestId("decrease-button");
+
+  const counterDiv = queryByTestId("div-counter");
+  fireEvent.click(increaseBtn);
+  fireEvent.click(increaseBtn);
+  fireEvent.click(increaseBtn);
+
+  fireEvent.click(decreaseBtn);
+
+  expect(counterDiv).toHaveTextContent("2");
+});
+
+test("counter will not be negtive number", () => {
+  const { queryByTestId } = screen;
+  const decreaseBtn = queryByTestId("decrease-button");
+  const counterDiv = queryByTestId("div-counter");
+  fireEvent.click(decreaseBtn);
+
+  expect(counterDiv).toHaveTextContent("0");
 });
